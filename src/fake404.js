b7e6842c3ea7db5389bcafd9fe8a0538611d21cb
ttxs25830 <57991009+ttxs25830@users.noreply.github.com>
2022-07-10 19:07:30 +0800

    update redict

--- src/fake404.js
+++ src/fake404.js
@@ -1,8 +1,18 @@
 import { fromUint8Array as b64Encode } from 'js-base64'
-const b64p = b64Encode(new Uint8Array(decodeURI(location.pathname)), true)
-const b64q = b64Encode(new Uint8Array(decodeURI(location.search.substring(1))), true)
-location.replace(encodeURI(
-  `${location.protocol}//${location.hostname}${
-    location.port ? ':' + location.port : ''
-  }/?b64p=${b64p}&b64q=${b64q}`
-))
+const b64p = b64Encode(
+  new Uint8Array(new TextEncoder().encode(decodeURI(location.pathname))),
+  true
+)
+const b64q = b64Encode(
+  new Uint8Array(
+    new TextEncoder().encode(decodeURI(location.search.substring(1)))
+  ),
+  true
+)
+location.replace(
+  encodeURI(
+    `${location.protocol}//${location.hostname}${
+      location.port ? ':' + location.port : ''
+    }/?b64p=${b64p}${b64q ? '&b64q=' + b64q : ''}`
+  )
+)
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -6,19 +7,19 @@ import { fromBase64 as b64Decode } from 'js-base64'
 
 createApp(App).use(store).use(router).mount('#app')
 
-const rPathParm = location.search.match(/(^|&)b64p=([^&]*)(&|$)/)
+const rPathParm = location.search.match(/(^|&|\?)b64p=([^&]*)(&|$)/)
 if (rPathParm) {
   const opath = b64Decode(rPathParm[2])
-  const rQueryParm = location.search.match(/(^|&)b64q=([^&]*)(&|$)/)
+  const rQueryParm = location.search.match(/(^|&|\?)b64q=([^&]*)(&|$)/)
   if (rQueryParm) {
-    const oquery = (parmlist = b64Decode(rQueryParm[2]).split('&')) => {
+    const oquery = ((parmlist) => {
       const dict = {}
       for (const i in parmlist) {
-        const pair = i.split('=')
+        const pair = parmlist[i].split('=')
         dict[pair[0]] = pair[1]
       }
       return dict
-    }
+    })(b64Decode(rQueryParm[2]).split('&'))
     router.push({
       path: opath,
       query: oquery
