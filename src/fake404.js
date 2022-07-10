575f153e6a11495d0f83df1288ed4cf05fcf6a0b
ttxs25830 <57991009+ttxs25830@users.noreply.github.com>
2022-07-10 18:28:58 +0800

    update b64

--- src/fake404.js
+++ src/fake404.js
@@ -1,6 +1,6 @@
-import Base64 from 'js-base64'
-const b64p = Base64.fromUint8Array(decodeURI(location.pathname), true)
-const b64q = Base64.fromUint8Array(decodeURI(location.search.substring(1)), true)
+import { fromUint8Array as b64Encode } from 'js-base64'
+const b64p = b64Encode(new Uint8Array(decodeURI(location.pathname)), true)
+const b64q = b64Encode(new Uint8Array(decodeURI(location.search.substring(1))), true)
 location.replace(encodeURI(
   `${location.protocol}//${location.hostname}${
     location.port ? ':' + location.port : ''
--- src/main.js
+++ src/main.js
@@ -2,24 +2,30 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Base64 from 'js-base64'
+import { fromBase64 as b64Decode } from 'js-base64'
 
 createApp(App).use(store).use(router).mount('#app')
 
-const opath = Base64.fromBase64(location.search.match(/(^|&)b64p=([^&]*)(&|$)/)[2])
-const oquery = (
-  parmlist = Base64.fromBase64(location.search.match(/(^|&)b64q=([^&]*)(&|$)/)[2]).split(
-    '&'
-  )
-) => {
-  const dict = {}
-  for (const i in parmlist) {
-    const pair = i.split('=')
-    dict[pair[0]] = pair[1]
+const rPathParm = location.search.match(/(^|&)b64p=([^&]*)(&|$)/)
+if (rPathParm) {
+  const opath = b64Decode(rPathParm[2])
+  const rQueryParm = location.search.match(/(^|&)b64q=([^&]*)(&|$)/)
+  if (rQueryParm) {
+    const oquery = (parmlist = b64Decode(rQueryParm[2]).split('&')) => {
+      const dict = {}
+      for (const i in parmlist) {
+        const pair = i.split('=')
+        dict[pair[0]] = pair[1]
+      }
+      return dict
+    }
+    router.push({
+      path: opath,
+      query: oquery
+    })
+  } else {
+    router.push({
+      path: opath
+    })
   }
-  return dict
 }
-router.push({
-  path: opath,
-  query: oquery || undefined
-})
