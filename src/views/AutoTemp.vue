9abb10ac58a5e148402b311e3f4de66d13dba963
ttxs25830 <57991009+ttxs25830@users.noreply.github.com>
2022-07-11 13:45:29 +0800

    update autotemp

--- src/views/AutoTemp.vue
+++ src/views/AutoTemp.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="about">
     <h1>本项目旨在帮同学们<del>糊弄</del>认真填写体温表格</h1>
-    <h3>本项目开源在<a href="https://github.com/ttxs25830/AutoTemp/tree/v0.0.1-beta">Github</a></h3>
-    <h3>无法访问Github的同学可以到<a href="https://www.123pan.com/s/zoRRVv-mQCWd">123云盘(提取码：001b)</a>直接下载可执行文件以及说明</h3>
+    <h3>本项目开源在<a href="https://github.com/ttxs25830/AutoTemp/tree/v0.0.2-beta">Github</a></h3>
+    <h3>无法访问Github的同学可以到<a href="https://www.123pan.com/s/zoRRVv-xQCWd">123云盘(提取码：002b)</a>直接下载可执行文件以及说明</h3>
+    <h3>最后更新：<strong>2022/7/11 下午</strong></h3>
   </div>
 </template>
